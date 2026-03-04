a314b847092a096c266ac2523f514268332a5107
JiriGalacz <92362859+JiriGalacz@users.noreply.github.com>
2026-03-04 19:41:28 +0200

    promazani duplicit a nefunkcnich casti kodu, zmeny v main ts spousteni casti kodu jen pokud jsou otevrene dane stranky

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,20 @@ import { initFormValidation } from './form.js';
 import { initScrollUpButton } from './utils.js';
 // POČKÁME, AŽ SE NAČTE CELÁ STRÁNKA (HTML)
 document.addEventListener('DOMContentLoaded', () => {
-    // Spustíme moduly. Vykonají se jen ty, které najdou na aktuální stránce své HTML elementy.
-    initGallery();
-    initFormValidation();
-    initScrollUpButton();
+    // 1. Galerie - spustíme jen na stránce, kde je kontejner pro filmy
+    // V gallery.ts hledáš ID 'movie-gallery'
+    if (document.getElementById('movie-gallery')) {
+        initGallery();
+    }
+    // 2. Registrační formulář - spustíme jen tam, kde se nachází
+    // Ve form.ts hledáš ID 'registration-form'
+    if (document.getElementById('registration-form')) {
+        initFormValidation();
+    }
+    // 3. Tlačítko nahoru - spustíme jen pokud je prvek přítomen v HTML
+    // V utils.ts hledáš třídu '.scroll-up-btn'
+    if (document.querySelector('.scroll-up-btn')) {
+        initScrollUpButton();
+    }
 });
 //# sourceMappingURL=main.js.map
